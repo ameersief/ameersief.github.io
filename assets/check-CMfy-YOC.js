e8475211efb88abe46601f86c3723203320de435
Ameer Sief <35783877+ameersief@users.noreply.github.com>
2026-04-23 13:43:00 +0400

    Add SEO case study pages and copy sweep

---
+++ assets/check-CMfy-YOC.js
@@ -0,0 +1,11 @@
+import{l as o}from"./index-VU2qjAqw.js";/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],a=o("arrow-left",c);/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],n=o("check",e);export{a as A,n as C};
